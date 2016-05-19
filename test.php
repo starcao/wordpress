f8eceba0bd9d990ad333e2bd67bd20fb4d3e8d9f
starcao <[476592125@qq.com]>
2016-05-19 09:36:40 +0800

    test.php

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * 递归删除文件夹
+ */
 function listDir($dir)
 {
     if(is_dir($dir))
@@ -7,49 +10,51 @@ function listDir($dir)
         {
             while(($file = readdir($dh)) !== false)
             {
-				if($file == '.svn')
-					removeDir($dir."/.svn/");
-				elseif(is_dir($dir."/".$file) && $file != "." && $file != "..")
-					listDir($dir."/".$file);
-			}		
+                if($file == '.svn')
+                    removeDir($dir."/.svn/");
+                elseif(is_dir($dir."/".$file) && $file != "." && $file != "..")
+                    listDir($dir."/".$file);
+            }
             closedir($dh);
         }
     }
 }
-
-function removeDir($dirName) 
-{ 
-    if(!is_dir($dirName)) 
-    { 
-        return false; 
-    } 
-    $handle = @opendir($dirName); 
-    while(($file = @readdir($handle)) !== false) 
-    { 
-        if($file != '.' && $file != '..') 
-        { 
-            $dir = $dirName . '/' . $file; 
-            is_dir($dir) ? removeDir($dir) : @unlink($dir); 
-        } 
-    } 
-    closedir($handle); 
-    return rmdir($dirName) ; 
+function removeDir($dirName)
+{
+    if(!is_dir($dirName))
+    {
+        return false;
+    }
+    $handle = @opendir($dirName);
+    while(($file = @readdir($handle)) !== false)
+    {
+        if($file != '.' && $file != '..')
+        {
+            $dir = $dirName . '/' . $file;
+            is_dir($dir) ? removeDir($dir) : @unlink($dir);
+        }
+    }
+    closedir($handle);
+    return rmdir($dirName) ;
 }
 
+/**
+ * 文件夹递归更新权限
+ */
 function uPermission($dir)
 {
-	if(is_dir($dir))
+    if(is_dir($dir))
     {
         if($dh = opendir($dir))
         {
             while(($file = readdir($dh)) !== false)
             {
-				if(is_dir($dir."/".$file) && $file != "." && $file != "..")
-				{
-					uPermission($dir."/".$file);
-					chmod($dir."/".$file, 0755);
-				}
-			}		
+                if(is_dir($dir."/".$file) && $file != "." && $file != "..")
+                {
+                    uPermission($dir."/".$file);
+                    chmod($dir."/".$file, 0755);
+                }
+            }
             closedir($dh);
         }
     }
